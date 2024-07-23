08e5839ca5de1d1a6263d5ae26ef887c3756a09e
Ranush Mithila <36297308+RanushMithila@users.noreply.github.com>
2024-07-23 23:00:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import asyncio
 from fastapi import FastAPI
-<<<<<<< HEAD
 from fastapi.staticfiles import StaticFiles
-=======
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
 from pydantic import BaseModel
 from typing import List
 from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
-<<<<<<< HEAD
 import base64
 import time
 from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-=======
-import sys
-import base64
-from fastapi.openapi.utils import get_openapi
-import yaml
-
-app = FastAPI()
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
 
 class URLItem(BaseModel):
     url: str
@@ -32,29 +20,13 @@ def extract_content(html_content):
     text_content = soup.get_text(separator=' ', strip=True)
     return html_content, text_content
 
-<<<<<<< HEAD
-# def image_to_base64(image_path):
-#     with open(image_path, "rb") as img_file:
-#         base64_image = base64.b64encode(img_file.read()).decode('utf-8')
-#     return base64_image
-=======
-def image_to_base64(image_path):
-    with open(image_path, "rb") as img_file:
-        base64_image = base64.b64encode(img_file.read()).decode('utf-8')
-    return base64_image
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
-
 async def take_screenshot(page, screenshot_path):
     await page.screenshot(path=screenshot_path)
 
 @app.post("/scrape/")
 async def scrape(url_item: URLItem):
     url = url_item.url
-<<<<<<< HEAD
     screenshot_path = 'screenshot-' + str(time.time()) +'.png'
-=======
-    screenshot_path = 'screenshot.png'
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
@@ -70,23 +42,13 @@ async def scrape(url_item: URLItem):
         
         # Take a screenshot of the webpage
         await take_screenshot(page, screenshot_path)
-<<<<<<< HEAD
-        
+
         await browser.close()
     screenshot_url = f"/static/{screenshot_path}"
     print(screenshot_url)
-=======
-        base64Image = image_to_base64(screenshot_path)
-        
-        await browser.close()
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
     
     return {
         "html_content": html_content,
         "text_content": text_content,
-<<<<<<< HEAD
         "downloadable_screenshot_path": screenshot_url
-=======
-        "screenshot_path": base64Image
->>>>>>> eeeeb8dcfad0f298925571b822cc0cc22202729d
     }
